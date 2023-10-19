b8a17f89bfd8f92fbc316016b2d9ef8c682db961
Rahul Gopinath <rahul@gopinath.org>
2023-10-18 22:28:17 -0700

    .

--- cfgsampler.cpp
+++ cfgsampler.cpp
@@ -191,7 +191,7 @@ void key_get_num_strings_at(int key, Grammar* grammar, int l_str, max_count_t at
   for (int i = 0; i < def->len; i++) {
     Rule* r = &def->rules[i];
     max_count_t v = rule_get_num_strings(key, i, 0, r->tokens, r->len, grammar, l_str);
-    if ((s + v) >= at) {
+    if ((s + v) > at) {
       //printf("reached\n");
       // at - s is the remainder of at that we need to reach.
       rule_get_num_strings_at(key, i, 0, r->tokens, r->len, grammar, l_str, at - s);
@@ -218,7 +218,7 @@ void rule_get_num_strings_at(int key, int rule, int pos, int* tokens, int len, G
   if (!len) return; // empty rule
   if (len == 1) {
     max_count_t v = key_get_num_strings(tokens[0], grammar, l_str); // if not tail
-    if (v >= at) {
+    if (v > at) { // if v == 0, we should not be going in.
       key_get_num_strings_at(tokens[0], grammar, l_str, at); // if not tail
     }
     return;
@@ -234,7 +234,7 @@ void rule_get_num_strings_at(int key, int rule, int pos, int* tokens, int len, G
     max_count_t rem = rule_get_num_strings(key, rule, pos+1, tail, len-1, grammar, l_str - l_str_x );
     max_count_t sr = s_ * rem;
 
-    if ((sum_rule + sr) >= at) {
+    if ((sum_rule + sr) > at) {
       // At this point, we know that we have overshot the `at`, which means that
       // our `at` is somewhere in the current rule. So, dig in.
       // There is a complication here. We overshot using the
@@ -272,8 +272,8 @@ void count_rules_at(int l_str, int sample) {
     key_get_num_strings_at(0, &g_grammar, l_str, sample);
     return;
   }
-  for (int i = 1; i < 100; i++) {
-    if (i > l_str) break;
+  for (int i = 1; i < count; i++) {
+    if (i > count) break;
     key_get_num_strings_at(0, &g_grammar, l_str, i); // this is l_str long
     printf("\n");
   }
